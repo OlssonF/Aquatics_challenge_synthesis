6427bac6b8225fd3dbfff3e635df2b69f9499f24
Freya E Olsson <freyao@tc014.cm.cluster>
2023-11-06 14:07:47 -0500

    gs model list

--- 01_get_model_list.R
+++ 01_get_model_list.R
@@ -7,6 +7,11 @@ library(lubridate)
 scores_s3 <- arrow::s3_bucket("neon4cast-scores/parquet/aquatics/",
                               endpoint_override= "data.ecoforecast.org", anonymous = T)
 
+googlesheets4::gs4_deauth()
+model_meta <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1oC7_w63wSCXNiHs1IK8AFGr0MG-NdjDAjwkfjvRZW-I/edit?usp=sharing") |> 
+  mutate(unc_driver = ifelse(uses_NOAA == 1, 1, 0))
+
+
 # What and when?
 forecast_dates <- as.character(seq.Date(as_date('2023-01-01'), Sys.Date(), 'day'))
 variables <- c('temperature', 'oxygen', 'chla')
@@ -15,9 +20,10 @@ variables <- c('temperature', 'oxygen', 'chla')
   # been scored since the beginning of the year
 this_year <- scores_s3 |>
   arrow::open_dataset() |>
-  filter(reference_datetime %in% forecast_dates) |>
+  filter(reference_datetime %in% forecast_dates,
+         model_id %in% model_meta$model_id) |>
   distinct(model_id, reference_datetime) |>
-  collect()
+  collect() 
 
 # calculate the total number of submissions and the most recent and filter to only ones with enough to compare
   # at least half of the forecast reference_datetimes specified
@@ -30,12 +36,12 @@ submissions <- this_year |>
   filter(n_submissions >= length(forecast_dates)/3)
 
 
-xgboost_ids <- unique(submissions$model_id)[str_detect(unique(submissions$model_id), 'xgboost')]
-omit <- c(xgboost_ids[2:length(xgboost_ids)],
-          'null')
+# xgboost_ids <- unique(submissions$model_id)[str_detect(unique(submissions$model_id), 'xgboost')]
+# omit <- c(xgboost_ids[2:length(xgboost_ids)],
+#           'null')
 
 submissions |>
-  filter(model_id %nin% omit) |>
+  # filter(model_id %nin% omit) |>
   write_csv('recent_submissions.csv')
 #######################################
 
@@ -45,14 +51,13 @@ submissions |>
 scores_s3 <- arrow::s3_bucket("neon4cast-scores/parquet/aquatics/",
                               endpoint_override= "data.ecoforecast.org", anonymous = T)
 
-models <- read_csv('recent_submissions.csv')$model_id
 
 # build a local parquet database from which to query
 scores_s3 |>
   arrow::open_dataset() |>
   dplyr::filter(reference_datetime %in% forecast_dates,
                 variable %in% variables,
-                model_id %in% models) |>
+                model_id %in% model_meta$model_id) |>
   arrow::write_dataset("scores", partitioning=c("model_id", "site_id"))
 
 #====================================================================#
