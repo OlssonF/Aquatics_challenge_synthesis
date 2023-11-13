945830dd77c499684c359c2c477f63ae68a2492a
Freya E Olsson <freyao@tc271.cm.cluster>
2023-11-13 11:49:15 -0500

    :pencil2:

--- 01_get_model_list.R
+++ 01_get_model_list.R
@@ -16,35 +16,6 @@ model_meta <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/
 forecast_dates <- as.character(seq.Date(as_date('2023-01-01'), Sys.Date(), 'day'))
 variables <- c('temperature', 'oxygen', 'chla')
 
-# Get a list of model_id and reference_datetime combinations that have
-  # been scored since the beginning of the year
-this_year <- scores_s3 |>
-  arrow::open_dataset() |>
-  filter(reference_datetime %in% forecast_dates,
-         model_id %in% model_meta$model_id) |>
-  distinct(model_id, reference_datetime) |>
-  collect() 
-
-# calculate the total number of submissions and the most recent and filter to only ones with enough to compare
-  # at least half of the forecast reference_datetimes specified
-
-submissions <- this_year |>
-  mutate(reference_datetime = as_date(reference_datetime)) |>
-  group_by(model_id) |>
-  summarise(most_recent = max(reference_datetime),
-            n_submissions = n()) |>
-  filter(n_submissions >= length(forecast_dates)/3)
-
-
-# xgboost_ids <- unique(submissions$model_id)[str_detect(unique(submissions$model_id), 'xgboost')]
-# omit <- c(xgboost_ids[2:length(xgboost_ids)],
-#           'null')
-
-submissions |>
-  # filter(model_id %nin% omit) |>
-  write_csv('recent_submissions.csv')
-#######################################
-
 # Build/update local database using the recent_submissions
 
 # Grab the data from the score bucket
