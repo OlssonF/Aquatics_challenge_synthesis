8a5087a538309aa7628a3233e8bd2a79b69dc972
Freya E Olsson <freyao@tc271.cm.cluster>
2023-11-13 11:49:34 -0500

    score stage2 vs obs air_temperature

--- 03_score_NOAA.R
+++ 03_score_NOAA.R
@@ -1,9 +1,11 @@
 library(tidyverse)
 library(score4cast)
 library(lubridate)
+library(neonUtilities)
 source('R/score_weather.R')
 source('R/ignore_sigpipe.R')
 
+# --- stage2 vs stage3 ----
 # want to score all this years forecasts
 # 1. Check the date
 dates <- seq.Date(as_date('2023-01-01'), (Sys.Date() - days(1)), 'day')
@@ -27,7 +29,7 @@ site_dates <- expand.grid(reference_datetime = dates,
 to_score <- site_dates |> 
   anti_join(scored_forecasts) 
 
-# forecast from 06-01-2023 is missing
+# forecast from a couple of dates are missing
 
 to_score <- to_score |> 
   filter(!reference_datetime %in% as_date(c('2023-01-06', '2023-01-19', '2023-05-22')))
@@ -61,6 +63,91 @@ for (i in 1:nrow(to_score)) {
     mutate(model_id = 'NOAA_daily',
            date = as_date(datetime))
 
+  arrow::write_dataset(scores, path = 'scores', format = 'parquet',
+                       partitioning=c("model_id", "site_id", "reference_datetime"))
+  message(i, '/', nrow(to_score), ' ---- scored ', site, ' ', forecast_date, ' ----')
+}
+
+# --- stage 2 vs observations ----
+# want to score all this years forecasts
+# 1. Check the date
+dates <- seq.Date(as_date('2023-01-01'), (Sys.Date() - days(1)), 'day')
+lake_sites <- c('BARC', 'CRAM', 'LIRO', 'PRLA', 'PRPO', 'SUGG', 'TOOK')
+
+# 2. Check if the forecast has already been scored
+if (!dir.exists('scores/model_id=NOAA_daily_obs')) {
+  dir.create(file.path('scores', 'model_id=NOAA_daily_obs'))
+}
+
+scored_forecasts <-
+  arrow::open_dataset('scores') |>
+  filter(model_id == 'NOAA_daily_obs', 
+         site_id %in% lake_sites) |>
+  distinct(reference_datetime, site_id) |>
+  collect() |>
+  mutate(reference_datetime = as_date(reference_datetime))
+
+# 3. Generate list of forecasts to be scored
+# aquatic_sites <- arrow::open_dataset('scores') |>
+#   distinct(site_id) |>
+#   pull()
+
+site_dates <- expand.grid(site_id = lake_sites,
+                          reference_datetime = dates)
+
+to_score <- site_dates |> 
+  anti_join(scored_forecasts) 
+
+# forecast from a couple of dates are missing
+
+to_score <- to_score |> 
+  filter(!reference_datetime %in% as_date(c('2023-01-06', '2023-01-19', '2023-05-22'))) 
+
+# Loop through all site-date combos to score
+for (i in 1:nrow(to_score)) {
+  # get the forecast
+  forecast_date <- to_score$reference_datetime[i]
+  site <- to_score$site_id[i]
+  
+  stage_2 <- neon4cast::noaa_stage2(start_date = forecast_date) |>
+    filter(site_id == site, 
+           variable == 'air_temperature') |> # scoring AT only
+    collect()|> 
+    mutate(datetime = as_date(datetime)) |> 
+    group_by(parameter, reference_datetime, datetime, site_id, variable, family) |> 
+    summarise(prediction = mean(prediction))
+  
+  # get the observations
+  obs_start <- format(forecast_date, "%Y-%m")
+  obs_end <- format(forecast_date + days(35), "%Y-%m")
+  
+  neon_obs <- loadByProduct(dpID="DP1.20046.001", 'BARC',
+                            startdate=obs_start, enddate=obs_end, 
+                            package="expanded", 
+                            token = Sys.getenv("NEON_TOKEN"),
+                            check.size = F,
+                            release = "current")
+  
+  # unlist the variables and add to the global environment
+  list2env(neon_obs, .GlobalEnv)
+  
+  #format data
+  neon_obs_dat <- RHbuoy_30min %>%
+    select(siteID, startDateTime, tempRHMean) %>%
+    mutate(datetime = as_date(startDateTime)) %>%
+    group_by(siteID, datetime) |> 
+    summarise(observation = mean(tempRHMean), .groups = 'drop') |> 
+    rename(site_id = siteID) %>%
+    select(site_id, datetime, observation) |> 
+    mutate(variable = 'air_temperature',
+           observation = observation + 273.15)
+  
+  
+  scores <- score_weather_forecast(forecast = stage_2, target = neon_obs_dat) |>
+    mutate(reference_datetime = as_date(reference_datetime),
+           model_id = 'NOAA_daily_obs',
+           date = as_date(datetime))
+  
   arrow::write_dataset(scores, path = 'scores', format = 'parquet',
                        partitioning=c("model_id", "site_id", "reference_datetime"))
   message(i, '/', nrow(to_score), ' ---- scored ', site, ' ', forecast_date, ' ----')
