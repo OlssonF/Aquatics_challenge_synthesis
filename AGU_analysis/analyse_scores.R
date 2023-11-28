84f0e112dd88a8d7aac8fd4da21567021a0293d1
Freya E Olsson <freyao@tc032.cm.cluster>
2023-11-28 11:03:24 -0500

    example forecast and rankings

--- AGU_analysis/analyse_scores.R
+++ AGU_analysis/analyse_scores.R
@@ -1,21 +1,145 @@
-knitr::opts_chunk$set(echo = TRUE)
+
 library(tidyverse)
 library(arrow)
 library(lubridate)
+library(ggh4x)
 
 # read in the all submissions ensemble for the lake sites
 lake_sites <- c('BARC', 'CRAM', 'LIRO', 'PRLA', 'PRPO', 'SUGG', 'TOOK')
 
-all_ensemble_scores <- arrow::open_dataset("scores_rescored") |>
+all_ensemble_scores <- arrow::open_dataset("scores_rescored/") |>
   filter(site_id %in% lake_sites,
-         model_id == 'all_submissions',
          variable == 'temperature') |>
-  collect()
+  collect() 
 
 all_ensemble_scores |> 
-  mutate(horizon = as_date(datetime) - as_date(reference_datetime)) |> 
-  group_by(horizon, site_id, model_id) |> 
+  mutate(horizon = as_date(datetime) - as_date(reference_datetime)) |>
+  group_by(model_id, site_id) |> 
   summarise(mean_crps = mean(crps, na.rm = T)) |> 
-  ggplot(aes(x=horizon, y= mean_crps)) +
-  geom_line(aes(colour = model_id)) +
+  group_by(site_id) |> 
+  slice_min(mean_crps, n= 5)
+
+googlesheets4::gs4_deauth()
+model_meta <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1oC7_w63wSCXNiHs1IK8AFGr0MG-NdjDAjwkfjvRZW-I/edit?usp=sharing")
+
+all_ensemble_scores |> 
+  mutate(horizon = as.numeric(as_date(datetime) - as_date(reference_datetime))) |>
+  filter(between(horizon, 0, 30), model_id %in% top10$model_id) |> 
+  group_by(horizon, model_id, site_id) |> 
+  summarise(mean_crps = mean(crps, na.rm = T)) |> 
+  ggplot(aes(x=horizon, y= mean_crps, colour = model_id)) +
+  geom_line() +
   facet_wrap(~site_id)
+
+design <- "
+  A#
+  BC
+  DE
+  FG
+"
+
+example_plot <- all_ensemble_scores |> 
+  filter(reference_datetime == '2023-07-01') |> 
+  mutate(horizon = as.numeric(as_date(datetime) - as_date(reference_datetime)),
+         site_id = factor(site_id, levels = c('TOOK', 'PRLA', 'PRPO', 'CRAM', 'LIRO', 'SUGG', 'BARC'))) |>
+  filter(between(horizon, 1, 30), 
+         model_id %in% c('flareGLM', 'xgboost_parallel', 'tg_randfor', 'cb_prophet', 'tg_lasso')) |> 
+  ggplot() +
+  geom_point(aes(x=datetime, y=observation, shape = 'Observation')) +
+  # geom_ribbon(aes(x=datetime, ymax=quantile97.5, ymin=quantile02.5, fill = model_id), alpha = 0.2)+
+  geom_line(aes(x=datetime, y= mean, colour = model_id)) +
+  facet_manual(vars(site_id), design = design, labeller = label_both) +
+  scale_shape_manual(values = 16, name = '') +
+  scale_x_datetime(breaks = '10 days', date_labels = '%d %b') +
+  theme_bw(base_size = 14)  +
+  labs(x = 'Date', y='Temperature (°C)', colour = 'Model', title = 'Forecasts generated on: July 1, 2023') 
+
+ggsave(example_plot, filename = './AGU_analysis/example_forecast.png', height = 19, width = 19, units = 'cm')
+
+#----- rankings ------
+n_forecasts_site <- all_ensemble_scores |> na.omit() |> 
+  distinct(reference_datetime, site_id) |> 
+  group_by(site_id) |> 
+  summarise(n=n())
+
+n_forecasts <- length(unique(all_ensemble_scores$reference_datetime))
+
+ranks <- all_ensemble_scores %>%
+  mutate(horizon = as_date(datetime) - as_date(reference_datetime)) |>
+  filter(horizon %in% c(1:30)) |>
+  
+  select(reference_datetime, horizon, site_id, model_id, crps) |>
+  arrange(reference_datetime, horizon, site_id, crps) |>
+  group_by(reference_datetime, horizon, site_id) |>
+  mutate(rank = row_number()) |>
+  
+  group_by(horizon, site_id, rank, model_id) |>
+  summarise(proportion = (n()/n_forecasts)) |>
+  ungroup() |>
+  
+    # make sure every rank is present in each model/site_id/horizon group
+  complete(rank, nesting(model_id, site_id, horizon), fill = list(proportion = 0))  
+
+ranks |> 
+  filter(model_id %in% c('flareGLM', 'fARIMA', 'tg_randfor', 'all_submissions', 'xgboost_parallel', 'tg_lasso')) |> 
+  mutate(group_rank = cut(rank,
+                          breaks = ceiling(seq(0, 38, length.out = 11)),
+                          labels = seq(1,10, length.out = 10))) |> 
+  group_by(group_rank, model_id, site_id, horizon) |> 
+  summarise(proportion = sum(proportion)) |> 
+  ggplot() +
+  geom_area(aes(x=horizon, y=proportion, fill=fct_rev(as.factor(group_rank))),
+            colour="black", stat = 'identity', position = 'stack') +
+  facet_grid2(site_id~factor(model_id),
+              axes = 'all', remove_labels = 'all') +
+  scale_fill_brewer(palette = 'RdBu', name = 'Rank group') +
+  theme_bw() +
+  theme(panel.spacing.x = unit(1.1, 'lines'),
+        panel.spacing.y = unit(1.1, 'lines')) +
+  coord_cartesian(ylim = c(0,1), xlim = c(1,30)) +
+  scale_y_continuous(expand = c(0,-0.10), name = 'Proportion of forecasts') +
+  scale_x_continuous(expand = c(0,0), breaks = c(1,7,14), name = 'Horizon (days)')
+
+
+#-------------------------------------------------#
+# ---- summarise across site -------
+
+
+all_ensemble_scores %>%
+  mutate(horizon = as_date(datetime) - as_date(reference_datetime)) |>
+  filter(horizon %in% c(1:30)) |>
+  # mean skill per forecast
+  group_by(reference_datetime, site_id, model_id) |> 
+  summarise(crps = mean(crps, na.rm = T)) |> 
+  
+
+  select(reference_datetime, site_id, model_id, crps) |>
+  arrange(reference_datetime, site_id, crps) |>
+  group_by(reference_datetime, site_id) |>
+  mutate(rank = row_number()) |>
+  group_by(site_id, rank, model_id) |>
+  summarise(proportion = (n()/n_forecasts)) |>
+  ungroup() |>
+  # make sure every rank is present in each model/site_id/horizon group
+  complete(rank, nesting(model_id, site_id), fill = list(proportion = 0)) |> 
+  filter(model_id %in% c('flareGLM', 'fARIMA', 'tg_randfor', 'all_submissions', 'xgboost_parallel', 'tg_lasso')) |> 
+  mutate(group_rank = cut(rank,
+                          breaks = ceiling(seq(0, 38, length.out = 11)),
+                          labels = seq(1,10, length.out = 10))) |> 
+  group_by(group_rank, model_id, site_id) |> 
+  summarise(proportion = sum(proportion)) |> 
+  ggplot() +
+  geom_area(aes(y=group_rank, x=proportion, fill=fct_rev(as.factor(group_rank))),
+            colour="black", stat = 'identity', position = 'stack') +
+  facet_grid2(site_id~factor(model_id),
+              axes = 'all', remove_labels = 'all') +
+  scale_fill_brewer(palette = 'RdBu', name = 'Rank group') +
+  theme_bw() +
+  theme(panel.spacing.x = unit(1.1, 'lines'),
+        panel.spacing.y = unit(1.1, 'lines')) +
+  coord_cartesian(ylim = c(0,1), xlim = c(1,30)) +
+  scale_y_continuous(expand = c(0,-0.10), name = 'Proportion of forecasts') +
+  scale_x_continuous(expand = c(0,0), breaks = c(1,7,14), name = 'Horizon (days)')
+
+
+
