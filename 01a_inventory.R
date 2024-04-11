86817e82c77daac4c13f7be4a0046835d4c30799
Freya E Olsson <freyao@tc007.cm.cluster>
2024-04-11 13:05:02 -0400

    inventory all models

--- 01a_inventory.R
+++ 01a_inventory.R
@@ -4,22 +4,60 @@ googlesheets4::gs4_deauth()
 model_meta <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1oC7_w63wSCXNiHs1IK8AFGr0MG-NdjDAjwkfjvRZW-I/edit?usp=sharing") |> 
   mutate(unc_driver = ifelse(uses_NOAA == 1, 1, 0)) 
 
+lake_sites <- c('BARC', 'CRAM', 'LIRO', 'PRLA', 'PRPO', 'SUGG', 'TOOK')
+# How many models?
+model_meta |>
+  mutate(tw = str_detect(variable, 'temperature'),
+         do = str_detect(variable, 'oxygen'),
+         chla = str_detect(variable, 'chla')) |> 
+  summarise(sum(tw), sum(do), sum(chla))
 
-# water temperature models only
-wt_meta <- model_meta |>
-  filter(str_detect(variable, 'temperature'))
+model_meta |> 
+  group_by(variable) |> 
+  summarise(n())
 
-# types of uncertainty represented
-# Number of models that contain each type
-wt_meta |> 
-  summarise(across(contains('unc'), ~ sum(.x, na.rm = T)))
 
-# How many sources are represented in each model?
-wt_meta |> 
-  select(model_id, contains('unc_')) |> 
-  mutate(unc_n = rowSums(pick(where(is.numeric)))) |> 
-  group_by(unc_n) |> 
-  summarise(n = n())
+# model classes by variable
+
+model_meta |> 
+  filter(str_detect(variable, 'temperature')) |> 
+  group_by(model_type) |> 
+  summarise(n())
+
+model_meta |> 
+  filter(str_detect(variable, 'oxygen')) |> 
+  group_by(model_type) |> 
+  summarise(n())
+
+model_meta |> 
+  filter(str_detect(variable, 'chla')) |> 
+  group_by(model_type) |> 
+  summarise(n())
+
+
+# How many forecasts?
+all_lake_forecasts <- arrow::open_dataset('scores_2023') |> 
+  filter(model_id %in% model_meta$model_id,
+         site_id %in% lake_sites) |> 
+  distinct(reference_datetime, variable, 
+           site_id, model_id) |> 
+  collect()
+
+all_lake_forecasts |> 
+  filter(variable == 'temperature') |> 
+  summarise(n())
+
+# use AT as a covariate?
+model_meta |> 
+  select(model_id, NOAA_var, variable) |>
+  mutate(tw = str_detect(variable, 'temperature'),
+         do = str_detect(variable, 'oxygen'),
+         chla = str_detect(variable, 'chla')) |> 
+  filter(str_detect(NOAA_var, 'air_temp')) |>
+  summarise(sum(tw), sum(do), sum(chla))
+
+
+
 
 # Which weather variables are used in each model?
 NOAA_vars <- c('air_temperature',
@@ -56,36 +94,28 @@ NOAA_summary |>
   group_by(variable, vars_n) |> 
   summarise(n = n())
 
+
+# looking at drivers by variable:
+wt_meta |> 
+  mutate(n_noaa = length(str_split_1(string = NOAA_var, pattern = ',')))
+
 wt_meta |> group_by(model_type) |> 
   summarise(n())
 
-# summary of submissions --------------------
-scores <- arrow::open_dataset('./scores_2023')
 
-distinct_submissions <- scores |> 
-  distinct(model_id, site_id, variable) |> 
-  collect()
 
-distinct_submissions |> 
-  group_by(model_id) |> 
-  distinct(site_id) |> 
-  summarise(site_n = n()) |> 
-  group_by(site_n) |> 
-  summarise(n())
 
-distinct_submissions |> 
-  group_by(model_id) |> 
-  distinct(variable) |> 
-  summarise(variable_n = n()) |> 
-  group_by(variable_n) |> 
-  summarise(n())
+# types of uncertainty represented
+# Number of models that contain each type
+wt_meta <- model_meta |> 
+  filter(str_detect(variable, 'temperature'))
+wt_meta |> 
+  summarise(across(contains('unc'), ~ sum(.x, na.rm = T)))
 
-# models for temperature only
-distinct_submissions |> 
-  group_by(model_id) |> 
-  distinct(variable) |> 
-  mutate(n = 1) |> 
-  pivot_wider(names_from = variable, values_from = n, values_fill = list(n=0)) |> 
-  filter(temperature == 0 & chla == 1 & oxygen == 0) |> 
-  ungroup() |> 
-  summarise(n())
+# How many sources are represented in each model?
+wt_meta |> 
+  select(model_id, contains('unc_')) |> 
+  mutate(unc_n = rowSums(pick(where(is.numeric)))) |> 
+  group_by(unc_n) |> 
+  summarise(n = n())
+
