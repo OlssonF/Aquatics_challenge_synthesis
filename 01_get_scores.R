907be8e0536e8735d710a408378f08699df74195
Freya Olsson <freyao@vt.edu>
2024-04-17 16:59:10 +0000

    comment out s3 instructions

--- 01_get_scores.R
+++ 01_get_scores.R
@@ -1,8 +1,6 @@
-# Get model_id for this years submissions
 
 library(tidyverse)
 library(lubridate)
-`%nin%` = Negate(`%in%`)
 
 # Get from data publication on Zenodo
 save_loc <- here::here()
@@ -11,30 +9,29 @@ download.file(url = "https://sandbox.zenodo.org/records/46145/files/scores_2023.
               destfile = file.path(save_loc,"scores.zip"), method = "curl")
 unzip(file.path(save_loc,"scores.zip"))
 
-scores_s3 <- 'scores_2023'
+
 
 # if using S3...
 # scores_s3 <- arrow::s3_bucket("neon4cast-scores/parquet/aquatics/",
 #                               endpoint_override= "data.ecoforecast.org", anonymous = T)
-
-
-googlesheets4::gs4_deauth()
-model_meta <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1oC7_w63wSCXNiHs1IK8AFGr0MG-NdjDAjwkfjvRZW-I/edit?usp=sharing") |> 
-  mutate(unc_driver = ifelse(uses_NOAA == 1, 1, 0))
-
-
-# What and when?
-forecast_dates <- as.character(seq.Date(as_date('2023-01-01'), as_date('2023-12-31'), 'day'))
-variables <- c('temperature', 'oxygen', 'chla')
-sites <- c('BARC', 'CRAM', 'LIRO', 'PRLA', 'PRPO', 'SUGG', 'TOOK')
-
-# build a local parquet database from which to query
-scores_s3 |>
-  arrow::open_dataset() |>
-  dplyr::filter(reference_datetime %in% forecast_dates,
-                variable %in% variables,
-                site_id %in% sites, 
-                model_id %in% model_meta$model_id) |>
-  arrow::write_dataset("scores_2023", partitioning=c("model_id", "site_id"))
+# scores_s3 <- 'scores_2023'
+# googlesheets4::gs4_deauth()
+# model_meta <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1oC7_w63wSCXNiHs1IK8AFGr0MG-NdjDAjwkfjvRZW-I/edit?usp=sharing") |> 
+#   mutate(unc_driver = ifelse(uses_NOAA == 1, 1, 0))
+# 
+# 
+# # What and when?
+# forecast_dates <- as.character(seq.Date(as_date('2023-01-01'), as_date('2023-12-31'), 'day'))
+# variables <- c('temperature', 'oxygen', 'chla')
+# sites <- c('BARC', 'CRAM', 'LIRO', 'PRLA', 'PRPO', 'SUGG', 'TOOK')
+# 
+# # build a local parquet database from which to query
+# scores_s3 |>
+#   arrow::open_dataset() |>
+#   dplyr::filter(reference_datetime %in% forecast_dates,
+#                 variable %in% variables,
+#                 site_id %in% sites, 
+#                 model_id %in% model_meta$model_id) |>
+#   arrow::write_dataset("scores_2023", partitioning=c("model_id", "site_id"))
 
 #====================================================================#
