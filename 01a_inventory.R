2952d8ca15a0a49c4cd2a5c5a0747ba83767d918
Freya Olsson <freyao@vt.edu>
2024-04-18 16:11:27 +0000

    update inventory

--- 01a_inventory.R
+++ 01a_inventory.R
@@ -1,10 +1,14 @@
 library(tidyverse)
-# inventory
+
+
+# Model submissions -------------------------------------------------------
+# Read in the google sheet of model descriptions
 googlesheets4::gs4_deauth()
 model_meta <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1oC7_w63wSCXNiHs1IK8AFGr0MG-NdjDAjwkfjvRZW-I/edit?usp=sharing") |> 
   mutate(unc_driver = ifelse(uses_NOAA == 1, 1, 0)) 
 
 lake_sites <- c('BARC', 'CRAM', 'LIRO', 'PRLA', 'PRPO', 'SUGG', 'TOOK')
+
 # How many models?
 model_meta |>
   mutate(tw = str_detect(variable, 'temperature'),
@@ -18,7 +22,6 @@ model_meta |>
 
 
 # model classes by variable
-
 model_meta |> 
   filter(str_detect(variable, 'temperature')) |> 
   group_by(model_type) |> 
@@ -35,18 +38,6 @@ model_meta |>
   summarise(n())
 
 
-# How many forecasts?
-all_lake_forecasts <- arrow::open_dataset('scores_2023') |> 
-  filter(model_id %in% model_meta$model_id,
-         site_id %in% lake_sites) |> 
-  distinct(reference_datetime, variable, 
-           site_id, model_id) |> 
-  collect()
-
-all_lake_forecasts |> 
-  filter(variable == 'temperature') |> 
-  summarise(n())
-
 # use AT as a covariate?
 model_meta |> 
   select(model_id, NOAA_var, variable) |>
@@ -57,65 +48,42 @@ model_meta |>
   summarise(sum(tw), sum(do), sum(chla))
 
 
-
-
-# Which weather variables are used in each model?
-NOAA_vars <- c('air_temperature',
-               'air_pressure',
-               'relative_humidity',
-               'surface_downwelling_longwave_flux_in_air',
-               'surface_downwelling_shortwave_flux_in_air', 
-               'precipitation_flux',
-               'eastward_wind',
-               'northward_wind')
-
-NOAA_summary <- wt_meta |> 
-  select(model_id, variable) 
-
-
-for (i in 1:length(NOAA_vars)) {
-  detect_var <- NOAA_vars[i]
-  
-  NOAA_summary <- 
-    NOAA_summary |> 
-    mutate(detect_var = str_detect(wt_meta$NOAA_var, detect_var)) 
-  
-  colnames(NOAA_summary)[2 + i] <- detect_var
-}
-
-# Number of models that contain each variable
-NOAA_summary |> 
-  select(-model_id) |>
-  group_by(variable) |> 
-  summarise_all(~sum(.x, na.rm = T))
-
-NOAA_summary |> 
-  mutate(vars_n = rowSums(pick(where(is.logical)))) |> 
-  group_by(variable, vars_n) |> 
-  summarise(n = n())
-
-
-# looking at drivers by variable:
-wt_meta |> 
-  mutate(n_noaa = length(str_split_1(string = NOAA_var, pattern = ',')))
-
-wt_meta |> group_by(model_type) |> 
-  summarise(n())
-
-
-
-
 # types of uncertainty represented
 # Number of models that contain each type
-wt_meta <- model_meta |> 
-  filter(str_detect(variable, 'temperature'))
-wt_meta |> 
+model_meta |>
+  mutate(tw = str_detect(variable, 'temperature'),
+         do = str_detect(variable, 'oxygen'),
+         chla = str_detect(variable, 'chla'))  |> 
+  filter(tw == T) |> 
   summarise(across(contains('unc'), ~ sum(.x, na.rm = T)))
 
+
 # How many sources are represented in each model?
-wt_meta |> 
+model_meta |>
+  mutate(tw = str_detect(variable, 'temperature'),
+         do = str_detect(variable, 'oxygen'),
+         chla = str_detect(variable, 'chla'))  |> 
+  filter(tw == T) |> 
   select(model_id, contains('unc_')) |> 
   mutate(unc_n = rowSums(pick(where(is.numeric)))) |> 
   group_by(unc_n) |> 
   summarise(n = n())
 
+
+# Scores database summary -------------------------------------------------
+# total forecasts - distinct model/site/var/refdate combos
+distinct_forecasts <- arrow::open_dataset("./scores") |> 
+  distinct(model_id, site_id, variable, reference_datetime) |> 
+  collect()
+
+# variable forecasts
+distinct_forecasts |> 
+  group_by(variable ) |>
+  summarise(n = n()) |> 
+  mutate(percent = 100*(n/nrow(distinct_forecasts)))
+
+# lake temp forecasts
+distinct_forecasts |> 
+  filter(site_id %in% lake_sites,
+         variable == 'temperature') |> 
+  summarise(n = n()) 
