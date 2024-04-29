24973a98ba47877e2fcbb666ae0ddcdb9881a82a
Freya E Olsson <freyao@tc015.cm.cluster>
2024-04-29 11:53:36 -0400

    restructure scripts

--- 00_get_scores.R
+++ 00_get_scores.R
@@ -7,7 +7,7 @@ options(timeout = 1000)
 # Get from data publication on Zenodo
 save_loc <- here::here()
 
-download.file(url = "https://sandbox.zenodo.org/records/48909/files/scores_v2.zip?download=1",
+download.file(url = "https://sandbox.zenodo.org/records/46413/files/scores_archive.zip?download=1",
               destfile = file.path(save_loc,"scores.zip"))
 #If this doesn't work try adding the method = "curl" argument
 
@@ -25,12 +25,13 @@ unzip(file.path(save_loc,"scores.zip"))
 # 
 # # What and when?
 # forecast_dates <- as.character(seq.Date(as_date('2023-01-01'), as_date('2023-12-31'), 'day'))
-# variables <- c('temperature', 'oxygen', 'chla')
+# variables <- c('temperature', 'oxygen')
 # sites <- c('BARC', 'CRAM', 'LIRO', 'PRLA', 'PRPO', 'SUGG', 'TOOK')
 # 
 # 
 # bad_forecasts <- expand.grid(model_id = c('flareGOTM','flareGOTM_noDA', 'flare_ler', 'flare_ler_baseline'),
-#                              site_id = "TOOK")
+#                              site_id = "TOOK",
+#                              variable = 'temperature')
 # # build a local parquet database from which to query
 # scores_s3 |>
 #   arrow::open_dataset() |>
@@ -38,7 +39,7 @@ unzip(file.path(save_loc,"scores.zip"))
 #                 variable %in% variables,
 #                 site_id %in% sites,
 #                 model_id %in% model_meta$model_id) |>
-#   anti_join(bad_forecasts) |> 
+#   anti_join(bad_forecasts) |>
 #   arrow::write_dataset("scores", partitioning=c("model_id"))
 
 #====================================================================#
